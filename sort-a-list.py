ad2cc5191eceba626ff7fd07020e1a53d6d035cd
Ankur Gohain <68453512+Kill-JoyCoder@users.noreply.github.com>
2024-07-10 22:44:04 +0530

    Update sort-a-list.py

--- sort-a-list.py
+++ sort-a-list.py
@@ -1,7 +1,7 @@
 import pandas
 import numpy as np
 def sort_values():
-    d=pandas.read_excel(data='List of Hackathon Names.xlsx', )
+    d=pandas.read_excel(data='List of Hackathon Names.xlsx')
     d.sort_values(by=['Name'], ascending=[True])
     d.sort_values(by=['First Date'], ascending=[True])
     d.sort_values(by=['Last Date'], ascending=[True])
