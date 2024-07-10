e3417fa264e22771ec3102666b6da567ab228400
Kill-JoyCoder <ankurgohain2005@gmail.com>
2024-07-10 11:45:40 +0530

    Commit for GHW '24

---
+++ sort-a-list.py
@@ -0,0 +1,11 @@
+import pandas
+import numpy as np
+def sort_values():
+    d=pandas.read_excel(data='List of Hackathon Names.xlsx', )
+    d.sort_values(by=['Name'], ascending=[True])
+    d.sort_values(by=['First Date'], ascending=[True])
+    d.sort_values(by=['Last Date'], ascending=[True])
+    
+
+sort_values()
+
